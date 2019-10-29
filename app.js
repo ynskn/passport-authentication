331048fff1936abce44f666abae2cfa9f798cd0c
Seokin Yoon <siyoon@connect.ust.hk>
2019-10-29 17:32:37 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+// Routes
+app.use('/', require('./routes/index'));
+
+// Routes
+app.use('/users', require('./routes/users'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
