6f7c95e5aad406e17fa98fb2c81e4fc23e317eda
Seokin Yoon <siyoon@connect.ust.hk>
2019-11-04 11:24:43 +0900

    final

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
 module.exports = {
     MongoURI: 'mongodb+srv://PerkyPig:Cre%40tivityi2@perkypig-ywng2.mongodb.net/test?retryWrites=true&w=majority'
 }
+
+//note: Authentication Error happened because I was using the wrong passcode: --> "user passcode", not the account passcode
